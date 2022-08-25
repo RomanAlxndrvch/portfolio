84d8849cadb6b2036dfadf06137cb4e49048e010
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-08-24 20:41:39 -0600

    1. Inc version

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -9,9 +9,14 @@ function Skills() {
             <div className={`${containerStyles.container} ${classes.skillsContainer}`}>
                 <h2 className={classes.title}>Skills </h2>
                 <div className={classes.skills}>
-                    <Skill/>
-                    <Skill/>
-                    <Skill/>
+                    <Skill title={'JS'}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'}/>
+
+                    <Skill title={'CSS'}
+                           description={'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old'}/>
+
+                    <Skill title={'REACT'}
+                           description={'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import classes from './Skill.module.css'
 
-function Skill() {
+type SkillPropsType = {
+    title: string
+    description: string
+}
+
+function Skill(props: SkillPropsType) {
     return (
         <div className={classes.skill}>
-
+            <div className={classes.icon}></div>
+            <h3>{props.title}</h3>
+            <span className="description">
+                {props.description}
+            </span>
         </div>
     )
 }
