9be896bd516408d591613703ab31fdfd1767c6cf
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-10-27 22:27:28 -0600

    About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import MyPic from '../../assets/images/myPhoto2.jpg'
+import classes from './About.module.css'
 
 export const About = () => {
+    const shortInfoContent = [
+        {info: "Birthday:", content: '04.12.1994'},
+        {info: "Location:", content: 'Lethbridge,AB,Canada'},
+        {info: "Email:", content: 'romantroian94@gmail.com'},
+        {info: "Study:", content: 'Vinnytsia National Technical University'},
+        {info: "Hobbies:", content: 'Computer games,Software engineering'},
+    ]
     return (
-        <div>
-            ABOUT
+        <div className={classes.aboutContainer}>
+            <img src={MyPic} alt="MyPic" className={classes.img}/>
+            <section className={classes.infoContainer}>
+                <div className={classes.aboutMe}>
+                    <h3>Roman Troian & Front-end developer</h3>
+                </div>
+                <div className={classes.shortInfo}>
+                    <table cellPadding="0" cellSpacing="0">
+                        <tbody>
+                        {shortInfoContent.map(el => {
+                            return <tr>
+                                <td className={classes.infoName}>{el.info}</td>
+                                <td className={classes.infoContent}>{el.content}</td>
+                            </tr>
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </div>
     )
 }
