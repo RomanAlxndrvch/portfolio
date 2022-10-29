8e0ac50b51fcbea7594cc7b1b52ccdb61a4e8661
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-10-29 13:18:24 -0600

    About+

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,17 +16,24 @@ export const About = () => {
             <section className={classes.infoContainer}>
                 <div className={classes.aboutMe}>
                     <h3>Roman Troian & Front-end developer</h3>
-                    <p className={classes.infoContent}>I’m a Front-End Developer located in Canada,AB.
-                        Well-organised person, problem solver,employee with high attention to detail. Fan of computer games, outdoor,movies and technologies. A family person and father of son,
-                        Interested in the entire frontend spectrum and working on ambitious projects with positive people.</p>
+                    <p style={{paddingBottom: '30px'}}>I’m a Front-End Developer located in Canada,AB.
+                        Well-organised person, problem solver,employee with high attention to detail. Fan of computer
+                        games, outdoor,movies and technologies. A family person and father of son,
+                        Interested in the entire frontend spectrum and working on ambitious projects with positive
+                        people.</p>
+                    <h3>Skills</h3>
+                    <p>JavaScript · TypeScript · CSS · HTML · React · Redux · Redux Toolkit · GitHub · LESS · SASS ·
+                        Rest API · Snapshot
+                        testing · Unit testing · Git · StoryBook · Material UI · Axios · Formik and many more </p>
                 </div>
+
                 <div className={classes.shortInfo}>
                     <table cellPadding="0" cellSpacing="0">
                         <tbody>
                         {shortInfoContent.map(el => {
                             return <tr>
                                 <td className={classes.infoName}>{el.info}</td>
-                                <td className={classes.infoContent}>{el.content}</td>
+                                <td>{el.content}</td>
                             </tr>
                         })}
                         </tbody>
