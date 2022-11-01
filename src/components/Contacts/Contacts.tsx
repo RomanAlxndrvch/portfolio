f24f81c20fc44b16d48effeeac5c39d143b17d35
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-10-31 22:58:16 -0600

    contact+

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,6 +1,12 @@
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React, {
+    ChangeEvent,
+    FormEvent, useEffect,
+    useRef,
+    useState
+} from "react";
 import classes from './Contact.module.css'
-import {Button, TextField} from "@mui/material";
+import {Alert, AlertTitle, Button, TextField} from "@mui/material";
+import emailjs from '@emailjs/browser';
 
 const buttonStyle = {
     width: 'min-content',
@@ -14,6 +20,7 @@ const buttonStyle = {
     }
 }
 
+
 export const Contacts = () => {
     const [error, setError] = useState<{ nameError: boolean, emailError: boolean, textError: boolean }>({
         nameError: false,
@@ -23,21 +30,7 @@ export const Contacts = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [text, setText] = useState('')
-
-    const onBtnClickHandler = () => {
-        if (name === '') {
-            return setError({...error, nameError: true})
-        }
-        else if (email === '') {
-            return setError({...error, emailError: true})
-        }
-        else if (text === '') {
-            return setError({...error, textError: true})
-        }
-        else {
-            return setError({...error, nameError: false, emailError: false, textError: false})
-        }
-    }
+    const [opasity, setOpacity] = useState(0)
 
     const nameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setError({...error, nameError: false})
@@ -51,11 +44,45 @@ export const Contacts = () => {
         setError({...error, textError: false})
         setText(e.currentTarget.value)
     }
+    let alertStyle = {
+        opacity: opasity,
+        top: '10%'
+    }
+
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_amfozvo', 'template_hfgm7br', form.current ? form.current : '', '3ptSRVgAiE4HCrxh7').then(res => {
+            setName('')
+            setEmail('')
+            setText('')
+            setOpacity(1)
+            setTimeout(() => {
+                setOpacity(0)
+            }, 2000)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        setError({...error, emailError: false})
+    }, [])
+
+    useEffect(() => {
+        email.length > 0 && setError({
+            ...error, emailError: !/\S+@\S+\.\S+/.test(email
+            )
+        })
+    }, [email])
 
 
     return (
         <div className={classes.contactsContainer}>
-
+            <Alert className={classes.alert} style={alertStyle} severity="success"> <AlertTitle>Thank You</AlertTitle>
+                <strong>Your message has been sent </strong></Alert>
             <div className={classes.formContainer}>
                 <h6> CONTACTS</h6>
                 <p className={classes.contactText}>Everyone always can contact me through my <a
@@ -63,26 +90,49 @@ export const Contacts = () => {
                     target={'_blank'}> LinkedIn </a>,or send me
                     email via this form : </p>
 
-                <TextField error={error.nameError} color={"secondary"} fullWidth
-                           id="outlined-basic"
-                           label="Name"
-                           variant="outlined" onChange={nameChangeHandler}/>
-                <TextField error={error.emailError} color={"secondary"} fullWidth
-                           id="outlined-basic" label="Email"
-                           variant="outlined"
-                           type='email' onChange={emailChangeHandler}/>
-                <TextField
-                    error={error.textError}
-                    color={"secondary"}
-                    fullWidth
-                    id="outlined-multiline-flexible"
-                    label="Your message"
-                    multiline
-                    rows={4}
-                    onChange={textChangeHandler}
-                />
-                <Button size={"large"} variant={"contained"}
-                        sx={buttonStyle} onClick={onBtnClickHandler}>Send</Button>
+                <form className={classes.formContainer} ref={form} onSubmit={sendEmail}>
+                    <TextField error={error.nameError}
+                               value={name}
+                               type="text"
+                               name="name"
+                               color={"secondary"}
+                               fullWidth
+                               id="outlined-basic"
+                               label="Name"
+                               variant="outlined"
+                               onChange={nameChangeHandler}/>
+
+                    <TextField error={error.emailError}
+                               value={email}
+                               name="email"
+                               color={"secondary"}
+                               fullWidth
+                               id="outlined-basic"
+                               label="Email"
+                               variant="outlined"
+                               type='email'
+                               onChange={emailChangeHandler}/>
+                    <TextField
+                        type="text"
+                        value={text}
+                        name='text'
+                        error={error.textError}
+                        color={"secondary"}
+                        fullWidth
+                        id="outlined-multiline-flexible"
+                        label="Your message"
+                        multiline
+                        rows={4}
+                        onChange={textChangeHandler}
+                    />
+                    <Button size={"large"}
+                            variant={"contained"}
+                            sx={buttonStyle}
+                            type={"submit"}
+                            disabled={name === '' || email === '' || text === ''}
+                    >Send</Button>
+                </form>
+
             </div>
         </div>
     )
