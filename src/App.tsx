3a1a533a2e8f332f7e2870642205a481030063fb
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2023-01-29 17:54:38 -0700

    started work with header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Navbar} from "./components/navbar/Navbar";
 import {HashRouter} from "react-router-dom";
 import {Content} from "./components/content/Content";
+import {Header} from "./components/Header/Header";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <div className="App">
                 <Navbar/>
                 <Content/>
+                <Header/>
             </div>
         </HashRouter>
 
